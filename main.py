dbe97dd46ab64cdb2a4d8140e853d83bbdc9af33
iamDo <trianoware@gmail.com>
2025-11-17 14:15:36 +0100

    CLI Interface scaffolding

---
+++ main.py
@@ -0,0 +1,42 @@
+import argparse
+
+
+def add_command(title):
+    pass
+
+def list_command():
+    pass
+
+def done_command(title):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="A to-do app for scatterbrained individuals."
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    add_parser = subparsers.add_parser("add", help="Add a new idea")
+    add_parser.add_argument("title", help="Title of the idea")
+
+    list_parser = subparsers.add_parser("list", help="List current ideas")
+
+    done_parser = subparsers.add_parser("done", help="Mark idea as done")
+    done_parser.add_argument("title", help="Title of the idea to mark as done")
+
+    args = parser.parse_args()
+
+    if args.command == "add":
+        add_command(args.title)
+    elif args.command == "list":
+        list_command()
+    elif args.command == "done":
+        done_command(args.title)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
